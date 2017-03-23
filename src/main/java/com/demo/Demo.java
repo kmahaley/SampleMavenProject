2d09e7262a6b4060877f7ad8d48e911d788b1c32
Mahaley, Kartik <Kartik.Mahaley@ncr.com>
2017-03-23 00:59:28 -0400

    initial spring boot project

---
+++ src/main/java/com/demo/Demo.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by km185223 on 3/23/17.
+ */
+@SpringBootApplication
+public class Demo {
+    public static void main(String[] args) {
+
+        SpringApplication.run(Demo.class, args);
+    }
+}
