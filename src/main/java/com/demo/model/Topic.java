1d1a1ed01389aa7c640d1eaf574500de0bbbf900
Mahaley, Kartik <Kartik.Mahaley@ncr.com>
2017-05-04 20:11:51 -0400

    Initial commit

---
+++ src/main/java/com/demo/model/Topic.java
@@ -0,0 +1,25 @@
+package com.demo.model;
+
+/**
+ *  Topic describes details in the format of name, id, description
+ *
+ * @author km185223
+ */
+
+public class Topic {
+    /**
+     * Name of the topic
+     */
+    private String name;
+
+    /**
+     * Short description of the topic
+     */
+    private String descr;
+
+    /**
+     * Id of the topic
+     */
+    private int id;
+
+}
