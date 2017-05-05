e91dd9cf09ac0edcd5e449062b1d8063e7e90da7
Mahaley, Kartik <Kartik.Mahaley@ncr.com>
2017-05-05 16:19:34 -0400

    Add methods to controller

--- src/main/java/com/demo/controller/TopicController.java
+++ src/main/java/com/demo/controller/TopicController.java
@@ -2,10 +2,13 @@ package com.demo.controller;
 
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import com.demo.model.Topic;
 import com.demo.service.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,11 +18,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class TopicController {
 
     @Autowired
-    TopicService topicService;
+    private TopicService topicService;
 
-    @RequestMapping(value="/topics")
+    @RequestMapping(value="/topics", method= RequestMethod.GET)
     public List<Topic> getTopics(){
         return topicService.getTopicsFromService();
+    }
+
+    @RequestMapping(value="/topics", method= RequestMethod.POST)
+    public Topic addTopics(Topic topic){
+        return topicService.addTopicsFromService(topic);
+    }
+
+    @RequestMapping(value="/topics/{id}", method= RequestMethod.GET)
+    public Topic getTopicsFromId(@PathParam("id") int id){
+        return null;
     }
 
 }
