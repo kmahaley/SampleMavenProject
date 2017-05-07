cd4f9273cf50c62449f1ba5b09d41092c09a9b60
Mahaley, Kartik <Kartik.Mahaley@ncr.com>
2017-05-06 20:25:25 -0400

    Modify config properties in YAML

--- src/main/java/com/demo/Demo.java
+++ src/main/java/com/demo/Demo.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Demo {
+    private Demo(){}
     public static void main(String[] args) {
 
         SpringApplication.run(Demo.class, args);
